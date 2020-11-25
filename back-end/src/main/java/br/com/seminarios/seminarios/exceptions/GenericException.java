8273941e1f12345fd43fbceba8afcb3df32a9c6a
Davi Queiroz <dvbarreira@hotmail.com>
2020-11-24 23:58:42 -0300

    Fix na generic exception

--- back-end/src/main/java/br/com/seminarios/seminarios/exceptions/GenericException.java
+++ back-end/src/main/java/br/com/seminarios/seminarios/exceptions/GenericException.java
@@ -3,7 +3,7 @@ package br.com.seminarios.seminarios.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "RCBE.PORTAL.ERRO.INTERNO.SERVIDOR")
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Erro interno")
 public class GenericException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
